c9ec0434d8fca6c46bb27e25142df9c70cbc4de6
dandumitriu33 <dandumitriu33@gmail.com>
2019-11-04 16:23:29 +0200

    Adjust printing.py and add export.py w/ report.txt

--- export.py
+++ export.py
@@ -1,2 +1,18 @@
+from printing import *
 
-# Export functions
+
+def export_to_file():
+    body_of_text = print_all()
+    with open('report.txt', 'wt') as f:
+        f.write(print_count_games() + '\n')
+    with open('report.txt', 'at') as f:
+        f.write(print_decide() + '\n')
+        f.write(print_get_latest() + '\n')
+        f.write(print_count_by_genre() + '\n')
+        f.write(print_get_line_number_by_title() + '\n')
+        f.write(print_sort_abc() + '\n')
+        f.write(print_get_genres() + '\n')
+        f.write(print_when_was_top_sold_fps() + '\n')
+
+
+export_to_file()
--- printing.py
+++ printing.py
@@ -1,20 +1,52 @@
 from reports import *
 
 THE_FILE = 'game_stat.txt'
+THE_YEAR = input('Enter the year to see if there are games from then on the list: ')
+THE_GENRE = input('Enter a genre to count how many games it has on the list: ')
+THE_TITLE = input('Enter a title to see what number it is on the list: ')
+
+
+def print_count_games():
+    return 'The total number of games is %d.' % count_games(THE_FILE)
+
+
+def print_decide():
+    return f'Are there games from {THE_YEAR} on the list? {str(decide(THE_FILE, THE_YEAR))}'
+
+
+def print_get_latest():
+    return 'The latest game on the list is %s.' % get_latest(THE_FILE)
+
+
+def print_count_by_genre():
+    return 'There are {} {} games.'.format(count_by_genre(THE_FILE, THE_GENRE), THE_GENRE)
+
+
+def print_get_line_number_by_title():
+    return '{} is number {} on the list.'.format(THE_TITLE, get_line_number_by_title(THE_FILE, THE_TITLE))
+
+
+def print_sort_abc():
+    return f'Here are the games, alphabetically ordered: {sort_abc(THE_FILE)}'
+
+
+def print_get_genres():
+    return f'Here are the genres, alphabetically ordered: {get_genres(THE_FILE)}'
+
+
+def print_when_was_top_sold_fps():
+    return 'The top most sold First-person shooter game was released in {}.'.format(when_was_top_sold_fps(THE_FILE))
 
 
 def print_all():
-    selected_year = input('Enter the year to see if there are games from then on the list: ')
-    selected_genre = input('Enter a genre to count how many games it has on the list: ')
-    selected_title = input('Enter a title to see what number it is on the list: ')
-    print('The total number of games is %d.' % count_games(THE_FILE))
-    print('Are there games from {} on the list?'.format(selected_year), decide(THE_FILE, selected_year))
-    print('The latest game on the list is %s.' % get_latest(THE_FILE))
-    print('There are {} {} games.'.format(count_by_genre(THE_FILE, selected_genre), selected_genre))
-    print('{} is number {} on the list.'.format(selected_title, get_line_number_by_title(THE_FILE, selected_title)))
-    print('Here are the games, alphabetically ordered:', sort_abc(THE_FILE))
-    print('Here are the genres, alphabetically ordered:', get_genres(THE_FILE))
-    print('The top most sold First-person shooter game was released in {}.'.format(when_was_top_sold_fps(THE_FILE)))
+    print(print_count_games())
+    print(print_decide())
+    print(print_get_latest())
+    print(print_count_by_genre())
+    print(print_get_line_number_by_title())
+    print(print_sort_abc())
+    print(print_get_genres())
+    print(print_when_was_top_sold_fps())
 
 
 print_all()
