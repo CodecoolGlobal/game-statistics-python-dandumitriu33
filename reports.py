ca3e5ef01c3fe5b51a2f0089b02ac4d41713bf79
dandumitriu33 <dandumitriu33@gmail.com>
2019-11-04 12:12:54 +0200

    Add the count_games function

--- reports.py
+++ reports.py
@@ -8,3 +8,8 @@ def file_to_dict(file_name):
             working_dict[line_counter] = values
             line_counter += 1
     return working_dict
+
+
+def count_games(file_name):
+    data_dict = file_to_dict(file_name)
+    return len(data_dict)
