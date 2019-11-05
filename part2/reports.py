f5ea4f5b7bd4a19180d0929e41ff08b73403d283
dandumitriu33 <dandumitriu33@gmail.com>
2019-11-05 08:10:05 +0200

    Add the file_to_dict data reading function

--- part2/reports.py
+++ part2/reports.py
@@ -1,2 +1,10 @@
-
-# Report functions
+def file_to_dict(file_name):
+    working_dict = {}
+    with open(file_name, 'rt') as f:
+        line_counter = 1
+        for line in f:
+            stats = line.strip().split('\t')
+            values = stats[:]
+            working_dict[line_counter] = values
+            line_counter += 1
+    return working_dict
