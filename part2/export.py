04648b8095473a1282419c71a7de55d63ef7c8aa
dandumitriu33 <dandumitriu33@gmail.com>
2019-11-05 10:08:02 +0200

    Add export.py and sample report

--- part2/export.py
+++ part2/export.py
@@ -1,2 +1,17 @@
+from printing import *
 
-# Export functions
+
+def export_to_file():
+    with open('part2_report.txt', 'wt') as f:
+        f.write(print_get_most_played() + '\n')
+    with open('part2_report.txt', 'at') as f:
+        f.write(print_sum_sold() + '\n')
+        f.write(print_get_selling_avg() + '\n')
+        f.write(print_count_longest_title() + '\n')
+        f.write(print_get_date_avg() + '\n')
+        f.write(print_get_game() + '\n')
+        f.write(print_count_grouped_by_genre() + '\n')
+        f.write(print_get_date_ordered() + '\n')
+
+
+export_to_file()
